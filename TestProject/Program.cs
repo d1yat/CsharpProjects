e9cb92975e8e7c45b673b6404a9eaae195264dd5
Akhirudin Sudiyat <me.sudiyat@gmail.com>
2025-11-28 09:54:58 +0000

    add program entry point

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string? name;
+        Console.WriteLine("What is your name? ");
+        string? readResult = Console.ReadLine();
+        if (readResult == null)
+        {
+            name = "Guest";
+        }
+        Console.WriteLine($"Hi, {name}!");
+    }
+}
